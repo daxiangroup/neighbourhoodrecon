acf71c5966a6125af525196c9cd0988f46bed7bc
Tyler Schwartz <ts@daxiangroup.com>
2012-06-04 21:34:07 -0400

    adding some error codes/messages for user actions

---
+++ application/config/errorcodes.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'user_failed_save' => 'NRError 100: There was a problem saving user object',
+    'user_id_must_be_set' => 'NRError 101: The user id must be set',
+    'user_id_must_be_numeric' => 'NRError 102: The user id must be numeric',
+    'user_not_found' => 'NRError 103: The supplied user id could not be found',
+);
